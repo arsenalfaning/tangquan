16f40aca0f6eae0c28a195136a8509f0020acd9e
arsenalfaning <765205025@qq.com>
2018-07-17 20:15:54 +0800

    sleep

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 type conf struct {
@@ -88,7 +89,7 @@ func ReadRequest(s io.ReadWriteCloser) (*request, error) {
 			copy(tmp, buf[0:n])
 			data = append(data, tmp)
 		}
-		data1 := bytes.Join(data, []byte(""))
+		data1 := bytes.Join(data, make([]byte, 0))
 		if bytes.HasSuffix(data1, end) { //判断是否请求读取结束
 			data2 := data1[0 : len(data1)-len(end)]
 			fmt.Println(string(data2))
@@ -101,6 +102,7 @@ func ReadRequest(s io.ReadWriteCloser) (*request, error) {
 
 func (h *MyHandler) Process(s io.ReadWriteCloser, request *request) error {
 	url := h.baseUrl + request.Url
+	fmt.Println(url)
 	r, e := http.NewRequest(request.Method, url, bytes.NewReader(request.Body))
 	if e != nil {
 		return e
@@ -113,22 +115,57 @@ func (h *MyHandler) Process(s io.ReadWriteCloser, request *request) error {
 		return e1
 	}
 	//开始写响应
-	buf := make([]byte, 512)
+	buf := make([]byte, 1024)
 	end := []byte("#end#")
-	s.Write([]byte(request.Url + "\n"))
+	data := make([][]byte, 0)
+	data = append(data, []byte(request.Url + "\n"))
 	for {
 		n, e2 := resp.Body.Read(buf)
 		if e2 != nil && e2 != io.EOF {
 			return e2
 		}
 		if n > 0 {
-			_, e3 := s.Write(buf[0:n])
-			if e3 != nil {
-				return e3
-			}
+			tmp := make([]byte, n)
+			copy(tmp, buf)
+			data = append(data, tmp)
+			//_, e3 := s.Write(buf[0:n])
+			//if e3 != nil {
+			//	return e3
+			//}
 		} else { //结束了
-			_, e3 := s.Write(end)
-			return e3
+			//_, e3 := s.Write(end)
+			//fmt.Println(m)
+			//return e3
+			break
 		}
 	}
+	data = append(data, end)
+	dataByte := bytes.Join(data, make([]byte, 0))
+	fmt.Println(string(dataByte))
+
+	fmt.Println(time.Now())
+	index := 0
+	count := 0
+	delta := 1024
+	for {
+		index2 := index + delta
+		if len(dataByte) - count < delta {
+			index2 = len(dataByte)
+		}
+		_, e3 := s.Write(dataByte[index:index2])
+		if e3 != nil {
+			return e3
+		}
+		time.Sleep(time.Millisecond * 800)
+		count += index2 - index
+		index = index2
+		if index2 == len(dataByte) {
+			break
+		}
+	}
+
+	fmt.Println(time.Now())
+
+	fmt.Println(len(dataByte))
+	return nil
 }
